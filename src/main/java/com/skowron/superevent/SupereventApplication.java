d3c2ce6dcc150565a22b3f8f40700e852f1d4126
skoomi <michal.skowron92@gmail.com>
2019-04-10 00:54:41 +0200

    first commit

---
+++ src/main/java/com/skowron/superevent/SupereventApplication.java
@@ -0,0 +1,13 @@
+package com.skowron.superevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupereventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupereventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skowron/superevent/SupereventApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skowron.superevent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupereventApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
